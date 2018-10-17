48a790ef1c91b185f289aaee19ea360f74cf895e
Dominique Felix <dom@doms-imac>
2018-10-16 22:31:20 -0400

    Working Delete Action

--- cop4655project/fahme003P07/fahme003P07/ViewController.swift
+++ cop4655project/fahme003P07/fahme003P07/ViewController.swift
@@ -88,8 +88,35 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView,
                             commit editingStyle: UITableViewCellEditingStyle,
-                            forRowAt indexPath: IndexPath){
-        
+                            forRowAt indexPath: IndexPath) {
+        // If the table view is asking to commit a delete command...
+        if editingStyle == .delete {
+            let item = myRandomNumber.numberTable[indexPath.row]
+            
+            let title = "Delete \(item.description)?"
+            let message = "Are you sure you want to delete this item?"
+            
+            let ac = UIAlertController(title: title,
+                                       message: message,
+                                       preferredStyle: .actionSheet)
+            
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            ac.addAction(cancelAction)
+            
+            let deleteAction = UIAlertAction(title: "Delete", style: .destructive,
+                                             handler: { (action) -> Void in
+                                                // Remove the item from the store
+                                                self.myRandomNumber.removeItem(item)
+                                                
+                                                // Also remove that row from the table view with an animation
+                                                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            })
+            ac.addAction(deleteAction)
+            
+            // Present the alert controller
+            present(ac, animated: true, completion: nil)
+        }
+
     }
     
     
